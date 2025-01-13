bc392e2bc3b582f4e181bb01a797c1bad2574533
vasu nageshri <vasu.nageshri@simformsolutions.com>
2025-01-13 17:52:35 +0530

    [Task - 8]: add example of extension

---
+++ extension.dart
@@ -0,0 +1,13 @@
+extension WeekendCheck on DateTime {
+  bool get isWeekend {
+    return this.weekday == DateTime.saturday || this.weekday == DateTime.sunday;
+  }
+}
+
+void main() {
+  DateTime date1 = DateTime(2025, 1, 13);  
+  print(date1.isWeekend);  
+
+  DateTime date2 = DateTime(2025, 1, 11);  
+  print(date2.isWeekend);  
+}
